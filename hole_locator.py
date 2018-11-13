2be47f19e11aebf31c46434a64ef7672883452c5
isaacolson <olsonites@gmail.com>
2018-11-13 17:39:26 -0600

    Update hole_locator.py

--- hole_locator.py
+++ hole_locator.py
@@ -3,7 +3,7 @@
 
 '''Take a photo.
 Take a photo using a USB or Raspberry Pi camera.
-
+'''
 
 import os
 from time import time, sleep
@@ -121,11 +121,8 @@ def rpi_camera_photo():
     except OSError:
         log("Raspberry Pi Camera not detected.", "error")
 
-'''
 if __name__ == '__main__':
     farmware_name = 'hole_locator'
-    
-   ''' 
     try:
         CAMERA = os.environ['camera']
     except (KeyError, ValueError):
@@ -135,5 +132,3 @@ if __name__ == '__main__':
         rpi_camera_photo()
     else:
         usb_camera_photo()
-        
-        '''
