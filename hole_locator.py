ae4994fdf35c88dea9177dd4fb3cc56cb61ba419
isaacolson <olsonites@gmail.com>
2018-11-13 18:49:06 -0600

    Update hole_locator.py

--- hole_locator.py
+++ hole_locator.py
@@ -113,7 +113,7 @@ def rpi_camera_photo():
     try:
         filename_path = upload_path(image_filename())
         retcode = call(
-            ["raspistill", "-w", "1080", "-h", "720", "-o", filename_path])
+            ["raspistill", "-w", "3280", "-h", "2464", "-o", filename_path])
         if retcode == 0:
             print("Image saved: {}".format(filename_path))
         else:
