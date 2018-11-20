2c40038a41d04329e0208416b15328903739a106
isaacolson <olsonites@gmail.com>
2018-11-20 00:08:19 -0600

    Update hole_locator.py

--- hole_locator.py
+++ hole_locator.py
@@ -74,12 +74,12 @@ def draw(img):
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
     template = cv2.imread('template_1.jpg',0)
-    device.log('Drawing...', 'success', ['toast'])
     w, h = template.shape[::-1]
     
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
     loc = np.where( res >= threshold)
+    device.log('Drawing...', 'success', ['toast'])
     
     for pt in zip(*loc[::-1]):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
